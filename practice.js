fb0b936a03308d370d11a0b65446d2425234256b
mkinfrared <mkinfrared@gmail.com>
2018-04-17 14:48:55 -0600

    completed the task

--- practice.js
+++ practice.js
@@ -1,169 +1,230 @@
 /* 
-  Once you complete a problem, refresh ./SpecRunner.html in your browser and check to see if the problem's test(s) are passing.
-  Passed tests will be indicated by a green circle.
-  Failed tests will be indicated by a red X.
+ Once you complete a problem, refresh ./SpecRunner.html in your browser and check to see if the problem's test(s) are passing.
+ Passed tests will be indicated by a green circle.
+ Failed tests will be indicated by a red X.
 
-  You can refresh the page at any time to re-run all the tests.
+ You can refresh the page at any time to re-run all the tests.
 
-  In this repo your job is to write functions to make each function call work properly.
+ In this repo your job is to write functions to make each function call work properly.
 
-  Here's an example of code that will be given to you:
+ Here's an example of code that will be given to you:
 
-  sayHi('Hi Katie', function(thingToSay){
-    alert(thingToSay);
-  });
+ sayHi('Hi Katie', function(thingToSay){
+ alert(thingToSay);
+ });
 
-  It would be your job to create the sayHi function:
+ It would be your job to create the sayHi function:
 
-  var sayHi = function(str, cb){
-    cb(str);
-  }
-*/
+ var sayHi = function(str, cb){
+ cb(str);
+ }
+ */
 
 ////////// PROBLEM 1 //////////
 
 /*
-  Write a function called first that takes in two parameters, an array and a callback function.
-  Then invoke the callback function, passing in the first element in the array as it's argument.
-*/
+ Write a function called first that takes in two parameters, an array and a callback function.
+ Then invoke the callback function, passing in the first element in the array as it's argument.
+ */
 
 // Code Here 
+function first(arr, cb) {
+	return cb(arr[0]);
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
-first(names, function(firstName){
-  console.log('The first name in names is ' + firstName);
-  return firstName;
+first(names, function (firstName) {
+	console.log('The first name in names is ' + firstName);
+	return firstName;
 });
 // Do not edit the code above.
-
 
 
 ////////// PROBLEM 2 //////////
 
 /*
-  Write a function called last that takes in an array and a callback function. 
-  Then invoke the callback, passing in the last element in the array as the argument.
-*/
+ Write a function called last that takes in an array and a callback function.
+ Then invoke the callback, passing in the last element in the array as the argument.
+ */
 
 //Code Here
 
+function last(arr, cb) { return cb(arr.pop()); }
+
 // Do not edit the code below.
-last(names, function(lastName){
-  console.log('The last name in names is ' + lastName);
-  return lastName;
+last(names, function (lastName) {
+	console.log('The last name in names is ' + lastName);
+	return lastName;
 });
 // Do not edit the code above.
-
 
 
 ////////// PROBLEM 3 //////////
 
 /*
-  Write a function called multiply that takes in three parameters: two numbers and a callback function.  
-  Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
-*/
+ Write a function called multiply that takes in three parameters: two numbers and a callback function.
+ Invoke the callback, passing in the product of the two numbers multiplied as the argument.
+ */
 
 //Code Here
 
+function multiply(a, b, cb) { cb(a * b); }
+
 // Do not edit the code below.
-multiply(4, 3, function(answer){
-  console.log('The answer is ' + answer); //should console.log 12
+multiply(4, 3, function (answer) {
+	console.log('The answer is ' + answer); //should console.log 12
 });
 // Do not edit the code above.
-
 
 
 ////////// PROBLEM 4 //////////
 
 /*
-  Write a function called contains that takes in three parameters: an array, a name and a callback.  
-  Check if the name exists in the array. 
-  If it does, invoke the callback with true as the argument. 
-  If the name does not exist, invoke the callback with false as the argument.
-*/
+ Write a function called contains that takes in three parameters: an array, a name and a callback.
+ Check if the name exists in the array.
+ If it does, invoke the callback with true as the argument.
+ If the name does not exist, invoke the callback with false as the argument.
+ */
 
 //Code Here 
 
+function contains(arr, name, cb) {
+	arr.forEach(function (elem) {
+		if (elem === name) { cb(true); }
+	});
+
+	cb(false);
+}
+
 // Do not edit the code below.
-contains(names, 'Colt', function(result){
-  if(result === true){
-    console.log('Colt is in the array');
-  } else {
-    console.log('Colt is not in the array');
-  }
+contains(names, 'Colt', function (result) {
+	if (result === true) {
+		console.log('Colt is in the array');
+	} else {
+		console.log('Colt is not in the array');
+	}
 });
 // Do not edit the code above.
-
 
 
 ////////// PROBLEM 5 //////////
 
 /*
-  Write a function called uniq that takes in an array and a callback function.
-  Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
-*/
+ Write a function called uniq that takes in an array and a callback function.
+ Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
+ */
 
 //Code Here
+function uniq(arr, cb) {
+
+	// NOT WORKING SOLUTION
+	
+	// arr.forEach(function findUn(elem, i) {
+	// 	if (arr.indexOf(elem, i + 1) >= 0) {
+	// 		console.log(elem);
+	// 		console.log(arr.indexOf(elem, i + 1),);
+	// 		console.log(arr.indexOf(elem, i + 1),);
+	// 		arr.splice(arr.indexOf(elem, i + 1), 1);
+	// 		findUn();
+	// 	}
+	// });
+
+	// SOLUTION #2 -- WORKING
+
+	// let result = arr.filter(function (elem, index) {
+	// 	if (arr.indexOf(elem, index + 1) < 0) {
+	// 		return elem;
+	// 	}
+	// });
+	// console.log(arr);
+	// cb(result);
+
+	// SOLUTION #3
+
+	let obj    = {},
+		result = [];
+	arr.forEach(function (elem) {
+		if (elem in obj) {
+			obj[elem] += 1;
+		} else {
+			obj[elem] = 1;
+		}
+	});
+	for (const objKey in obj) {
+		result.push(objKey);
+	}
+	cb(result);
+}
 
 // Do not edit the code below.
-uniq(names, function(uniqArr){
-  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+uniq(names, function (uniqArr) {
+	console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
 // Do not edit the code above.
-
 
 
 ////////// PROBLEM 6 //////////
 
 /* 
-  Write a function called each that takes in an array of names and a callback function. 
-  For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
-*/
+ Write a function called each that takes in an array of names and a callback function.
+ For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
+ */
 
 //Code Here 
+function each(arr, cb) {
+	arr.forEach(function (elem, i) {
+		cb(elem, i);
+	});
+}
 
 // Do not edit the code below.
-each(names, function(item, indice){
-  console.log('The item in the ' + indice + ' position is ' + item)
+each(names, function (item, indice) {
+	console.log('The item in the ' + indice + ' position is ' + item)
 });
 // Do not edit the code above.
-
 
 
 ////////// PROBLEM 7 //////////
 
 /*
-  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
-  When the correct user object is found, invoke the callback with the user object as an argument.
-*/
+ Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
+ When the correct user object is found, invoke the callback with the user object as an argument.
+ */
 
 // Code here
+function getUserById(arr, id, cb) {
+	arr.forEach(function (elem) {
+		if (elem.id === id) {
+			cb(elem);
+		}
+	})
+}
 
 // Do not edit the code below.
 var users = [
-  {
-    id: '12d',
-    email: 'tyler@gmail.com',
-    name: 'Tyler',
-    address: '167 East 500 North'
-  },
-  {
-    id: '15a',
-    email: 'cahlan@gmail.com',
-    name: 'Cahlan',
-    address: '135 East 320 North'
-  },
-  {
-    id: '16t',
-    email: 'ryan@gmail.com',
-    name: 'Ryan',
-    address: '192 East 32 North'
-  },
+	{
+		id     : '12d',
+		email  : 'tyler@gmail.com',
+		name   : 'Tyler',
+		address: '167 East 500 North'
+	},
+	{
+		id     : '15a',
+		email  : 'cahlan@gmail.com',
+		name   : 'Cahlan',
+		address: '135 East 320 North'
+	},
+	{
+		id     : '16t',
+		email  : 'ryan@gmail.com',
+		name   : 'Ryan',
+		address: '192 East 32 North'
+	},
 ];
 
-getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+getUserById(users, '16t', function (user) {
+	console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
 });
 // Do not edit the code above.
